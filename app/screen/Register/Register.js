237225188b06a25526afd05b59f08a5df73e0eca
jonalyne <jonalynegarde@gmail.com>
2022-08-30 17:50:47 +0800

    Fixed TextInput

--- app/screen/Register/Register.js
+++ app/screen/Register/Register.js
@@ -3,8 +3,21 @@ import React from 'react'
 import useRegister from './useRegister'
 
 export default function Register() {
+  const genderSelection = [{
+    value: 'Male',
+  }, {
+    value: 'Female',
+    }
+];
 
-  const {firstname, handleFirstname} = useRegister()
+  const {
+    firstname, handleFirstname,
+    middlename, handleMiddlename,
+    lastname, handleLastname,
+    age, handleAge,
+    gender, handleGender,
+    submit
+  } = useRegister()
   return (
     <ScrollView style={styles.scroll}>
     <View style = {styles.body}>
@@ -13,17 +26,17 @@ export default function Register() {
 
       <View style = {styles.inputGroup}>
         <TextInput style = {styles.input} placeholder = "Firstname" defaultValue={firstname} onChangeText={handleFirstname}></TextInput>
-        <TextInput style = {styles.input} placeholder = "Middlename"></TextInput>
-        <TextInput style = {styles.input} placeholder = "Lastname"></TextInput>
-        <TextInput style = {styles.input} placeholder = "Age"></TextInput>
-        <TextInput style = {styles.input} placeholder = "Birthday"></TextInput>
-        <TextInput style = {styles.input} placeholder = "Gender"></TextInput>
+        <TextInput style = {styles.input} placeholder = "Middlename" defaultValue={middlename} onChangeText={handleMiddlename}></TextInput>
+        <TextInput style = {styles.input} placeholder = "Lastname" defaultValue={lastname} onChangeText={handleLastname}></TextInput>
+        <TextInput style = {styles.input} placeholder = "Age" defaultValue={age} onChangeText={handleAge}></TextInput>
+        <TextInput style = {styles.input} placeholder = "Birthday" ></TextInput>
+        
         <TextInput style = {styles.input} placeholder = "Phone Number"></TextInput>
         <TextInput style = {styles.input} placeholder = "Username"></TextInput>
         <TextInput style = {styles.input} placeholder = "Password"></TextInput>
         <TextInput style = {styles.input} placeholder = "Retype Password"></TextInput>
 
-        <Button title='Lets Go' onPress={()=>Alert.alert("Login me")}/>
+        <Button title='Lets Go' onPress={submit}/>
 
 
 
--- app/screen/Register/useRegister.js
+++ app/screen/Register/useRegister.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
+const base_url = "http://192.168.254.114:4000"
 export default function useRegister() {
   const [firstname, setFirstname] = useState("")
   const [middlename, setMiddlename] = useState("")
@@ -13,35 +15,50 @@ export default function useRegister() {
   const [retypePassword, setRetypePassword] = ("")
 
 
-  const handleFirstname=(input)=>{
+  const handleFirstname = (input) => {
+    console.log(input)
     setFirstname(input)
   }
-  const handleMiddlename=(input)=>{
+  const handleMiddlename = (input) => {
     setMiddlename(input)
+    console.log(input)
   }
-  const handleLastname=(input)=>{
+  const handleLastname = (input) => {
     setLastname(input)
+    console.log(input)
   }
-  const handleGender=(input)=>{
+  const handleGender = (input) => {
     setGender(input)
   }
-  const handleAge=(input)=>{
+  const handleAge = (input) => {
     setAge(input)
   }
-  const handleBirthday =(input)=>{
+  const handleBirthday = (input) => {
     setBirthday(input)
   }
-  const handlePhone = (input) =>{
+  const handlePhone = (input) => {
     setPhone(input)
   }
-  const handleUsername = (input) =>{
+  const handleUsername = (input) => {
     setUsername(input)
   }
-  const handlePassword = (input) =>{
+  const handlePassword = (input) => {
     setPassword(input)
   }
-  const handleRetypePassword =(input) => {
+  const handleRetypePassword = (input) => {
     setRetypePassword(input)
+  }
+
+  const submit = async() => {
+    console.log("Submiting")
+    const registerRequest = await axios.post(base_url + "/passenger/register", {
+      firstname,
+      middlename,
+      lastname
+    })
+
+    console.log(registerRequest.headers)
+
   }
 
   return {
@@ -54,6 +71,7 @@ export default function useRegister() {
     username, handleUsername,
     phone, handlePhone,
     password, handlePassword,
-    retypePassword, handleRetypePassword
-}
+    retypePassword, handleRetypePassword,
+    submit
+  }
 }
